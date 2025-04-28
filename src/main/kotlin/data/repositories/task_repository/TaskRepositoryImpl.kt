b2b98caef51d6350abd37f5b1e19be8c5b2553a4
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-28 16:52:23 +0300

    enhance TaskRepository - fix return type of some functions in TaskDataSource and TaskRepositoryImpl - injection TaskDataSource and LogDataSource to TaskRepositoryImpl by dependency injection principle

--- src/main/kotlin/data/repositories/task_repository/TaskRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/task_repository/TaskRepositoryImpl.kt
@@ -1,9 +1,14 @@
 package org.example.data.repositories.task_repository
 
+import org.example.data.datasource.log_data_source.LogDataSource
+import org.example.data.datasource.task_data_source.TaskDataSource
 import org.example.logic.repositories.task_repository.TaskRepository
 import org.example.models.Task
 
-class TaskRepositoryImpl :TaskRepository {
+class TaskRepositoryImpl (
+    taskDataSource: TaskDataSource,
+    logDataSource: LogDataSource
+) :TaskRepository {
     override fun createTask(task: Task) {
         TODO("Not yet implemented")
     }
@@ -13,6 +18,14 @@ class TaskRepositoryImpl :TaskRepository {
     }
 
     override fun deleteTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override fun fetchAllTasks(): List<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override fun fetchTask(): Task {
         TODO("Not yet implemented")
     }
 }
