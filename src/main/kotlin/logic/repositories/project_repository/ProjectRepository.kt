a7b9d1881017826fc730469cb7993eb4785dc682
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-28 16:54:52 +0300

    enhance ProjectRepository - fix return type of some functions in ProjectDataSource and ProjectRepositoryImpl - injection ProjectDataSource and LogDataSource to ProjectRepositoryImpl by dependency injection principle

--- src/main/kotlin/logic/repositories/project_repository/ProjectRepository.kt
+++ src/main/kotlin/logic/repositories/project_repository/ProjectRepository.kt
@@ -1,7 +1,14 @@
 package org.example.logic.repositories.project_repository
 
+import org.example.models.Project
+
 interface ProjectRepository {
     fun createProject()
-    fun editProject()
-    fun deleteProject()
+    fun editProject(project: Project)
+    fun deleteProject(project: Project)
+    fun fetchAllProjects() : List<Project>
+    fun fetchProject() : Project
+    fun addStateToProject(projectId: String, state: String): Result<Project>
+    fun editStateToProject(projectId: String, state: String): Result<Project>
+    fun removeStateFromProject(projectId: String, state: String): Result<Project>
 }
