2bb37b1357599b11be63b884f06847a6db653d17
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-29 23:26:37 +0300

    set up ProjectRepositoryImpl test class with mocked dependencies

--- src/test/kotlin/data/repositories/project_repository/ProjectRepositoryImplTest.kt
+++ src/test/kotlin/data/repositories/project_repository/ProjectRepositoryImplTest.kt
@@ -1,5 +1,24 @@
 package data.repositories.project_repository
 
- class ProjectRepositoryImplTest{
 
- }
+import io.mockk.mockk
+import org.example.data.datasource.log_data_source.LogDataSource
+import org.example.data.datasource.project_data_source.ProjectDataSource
+import org.example.data.repositories.project_repository.ProjectRepositoryImpl
+import org.example.models.Project
+import org.junit.jupiter.api.BeforeEach
+
+class ProjectRepositoryImplTest {
+    private var logDataSource: LogDataSource = mockk(relaxed = true)
+    private var projectDataSource: ProjectDataSource = mockk(relaxed = true)
+    private lateinit var projectRepositoryImpl: ProjectRepositoryImpl
+    private var project: Project = mockk()
+
+    @BeforeEach
+    fun setup() {
+        projectRepositoryImpl = ProjectRepositoryImpl(
+            logDataSource = logDataSource,
+            projectDataSource = projectDataSource
+        )
+    }
+}
