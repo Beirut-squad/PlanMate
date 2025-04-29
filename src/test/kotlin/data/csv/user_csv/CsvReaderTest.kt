78a583010ed4d209827bc3a3f85898039440d5db
Ismail Elkhalili <ismail.elkalili@gmail.com>
2025-04-29 19:33:26 +0300

    test : given CSV lines with some invalid entries, when read called, then return only valid Users

--- src/test/kotlin/data/csv/user_csv/CsvReaderTest.kt
+++ src/test/kotlin/data/csv/user_csv/CsvReaderTest.kt
@@ -10,6 +10,7 @@ import kotlin.test.BeforeTest
 import kotlin.test.Test
 import com.google.common.truth.Truth.assertThat
 import creator_helper.createCsvLineForUser
+import creator_helper.createCsvLineForUserInvalid
 import io.mockk.every
 import org.example.models.Role
 
@@ -42,4 +43,23 @@ class CsvReaderTest{
         // then
         assertThat(result).isEqualTo(listOf(user1, user2))
     }
- }
+
+    @Test
+    fun `given CSV lines with some invalid entries, when read called, then return only valid Users`() {
+        // given
+        val lines = listOf(
+            createCsvLineForUserInvalid(),
+            createCsvLineForUser()
+        )
+        val user = User(UUID.fromString("550e8400-e29b-41d4-a716-446655440000"), "Ismail", "secret123", "ismail.elkalili@gmail.com", Role.ADMIN, false)
+
+        every { parser.parseLine(lines[0]) } returns null
+        every { parser.parseLine(lines[1]) } returns user
+
+        // when
+        val result = reader.read(lines)
+
+        // then
+        assertThat(result).isEqualTo(listOf(user))
+    }
+}
