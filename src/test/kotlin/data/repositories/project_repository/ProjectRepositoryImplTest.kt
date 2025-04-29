7cee678d90dc8cc3968e68572302d6caa1811900
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-29 23:28:03 +0300

    verify success result when project creation succeeds

--- src/test/kotlin/data/repositories/project_repository/ProjectRepositoryImplTest.kt
+++ src/test/kotlin/data/repositories/project_repository/ProjectRepositoryImplTest.kt
@@ -1,12 +1,18 @@
 package data.repositories.project_repository
 
 
+import creator_helper.createProjectLogHelper
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.example.data.datasource.log_data_source.LogDataSource
 import org.example.data.datasource.project_data_source.ProjectDataSource
 import org.example.data.repositories.project_repository.ProjectRepositoryImpl
+import org.example.logic.exceptions.ProjectNotCreatedException
 import org.example.models.Project
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class ProjectRepositoryImplTest {
     private var logDataSource: LogDataSource = mockk(relaxed = true)
@@ -21,4 +27,19 @@ class ProjectRepositoryImplTest {
             projectDataSource = projectDataSource
         )
     }
+
+    //CreateProject Test
+    @Test
+    fun `should return success when project creation succeeds`() {
+        // Given
+        every { projectDataSource.createProject(project) } returns
+                Result.success("Project Created")
+        // When
+        val result = projectRepositoryImpl.createProject(project)
+
+        // Then
+        assertTrue(result.isSuccess)
+        verify(exactly = 1) { projectDataSource.createProject(project) }
+    }
+
 }
