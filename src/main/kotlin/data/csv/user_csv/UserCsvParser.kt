c3fed575b1e9f9287d518a3299e15c0b1e316957
Ismail Elkhalili <ismail.elkalili@gmail.com>
2025-04-29 17:25:18 +0300

    implement parseLine in userCsvParser to convert CSV line into User object

--- src/main/kotlin/data/csv/user_csv/UserCsvParser.kt
+++ src/main/kotlin/data/csv/user_csv/UserCsvParser.kt
@@ -7,7 +7,15 @@ import java.util.*
 
 class UserCsvParser: CsvParser<User> {
     override fun parseLine(line: String): User {
-        TODO("Not yet implemented")
+        val fields = line.split(",").map { it.trim() }
+        return User(
+            id = UUID.fromString(fields[0]),
+            name = fields[1],
+            password = fields[2],
+            email = fields[3],
+            role = Role.valueOf(fields[4]),
+            isDeleted = fields[5].toBoolean()
+        )
     }
 
     override fun parseFile(csvLines: List<String>): List<User> {
