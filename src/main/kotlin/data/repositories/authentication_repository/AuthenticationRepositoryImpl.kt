34e5d0f5441884a44af84d7a47b3f016440ed1d6
Zyad <zyadhammad531@gmail.com>
2025-04-30 14:30:02 +0300

    add repository test and implementation

--- src/main/kotlin/data/repositories/authentication_repository/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/authentication_repository/AuthenticationRepositoryImpl.kt
@@ -1,26 +1,29 @@
 package org.example.data.repositories.authentication_repository
 
+import org.example.data.datasource.authentication_data_source.AuthenticationDataSource
 import org.example.logic.repositories.authentication_repository.AuthenticationRepository
 import org.example.models.User
 
-class AuthenticationRepositoryImpl : AuthenticationRepository {
+class AuthenticationRepositoryImpl(
+    private val authenticationDataSource: AuthenticationDataSource
+) : AuthenticationRepository {
     override fun login(email: String, password: String): Result<User> {
-        TODO("Not yet implemented")
+        return authenticationDataSource.login(email = email, password = password)
     }
 
     override fun register(name: String, password: String, email: String): Result<User> {
-        TODO("Not yet implemented")
+        return authenticationDataSource.register(email = email, password = password, name = name)
     }
 
     override fun registerAdmin(name: String, password: String, email: String): Result<User> {
-        TODO("Not yet implemented")
+        return authenticationDataSource.registerAdmin(email = email, password = password, name = name)
     }
 
     override fun logout(): Result<Unit> {
-        TODO("Not yet implemented")
+        return authenticationDataSource.logout()
     }
 
     override fun checkIfFirstRegister(): Result<Unit> {
-        TODO("Not yet implemented")
+        return authenticationDataSource.checkIfFirstRegister()
     }
 }
