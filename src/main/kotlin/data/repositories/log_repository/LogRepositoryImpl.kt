90f7cc0f45842e108e82260a03b97f6e76307948
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-04-30 22:09:59 +0300

    fix log repository impl

--- src/main/kotlin/data/repositories/log_repository/LogRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/log_repository/LogRepositoryImpl.kt
@@ -1,19 +1,24 @@
 package org.example.data.repositories.log_repository
 
 import org.example.logic.repositories.log_repository.LogRepository
-import org.example.models.Log
+import org.example.models.ProjectLog
+import org.example.models.TaskLog
 import java.util.*
 
 class LogRepositoryImpl : LogRepository {
-    override fun getProjectHistory(id: UUID): List<Log> {
+    override fun getProjectHistory(id: UUID): List<ProjectLog> {
         TODO("Not yet implemented")
     }
 
-    override fun getTaskHistory(id: UUID): List<Log> {
+    override fun getTaskHistory(id: UUID): List<TaskLog> {
         TODO("Not yet implemented")
     }
 
-    override fun createLog(log: Log) {
+    override fun createTaskLog(taskLog: TaskLog) {
+        TODO("Not yet implemented")
+    }
+
+    override fun createProjectLog(projectLog: ProjectLog) {
         TODO("Not yet implemented")
     }
 
