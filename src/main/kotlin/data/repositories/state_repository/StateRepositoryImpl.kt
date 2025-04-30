f2341ae0839cfd8c1bde351b645195e3661e5c22
YaseenOmar <yz456359@gmail.com>
2025-04-30 13:33:51 +0300

    add test cases to StateRepositoryImpl

--- src/main/kotlin/data/repositories/state_repository/StateRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/state_repository/StateRepositoryImpl.kt
@@ -1,19 +1,24 @@
 package org.example.data.repositories.state_repository
 
+import data.datasource.state_data_source.StateDataSource
 import org.example.logic.repositories.state_repository.StateRepository
 import org.example.models.State
+import java.util.UUID
 
 
-class StateRepositoryImpl : StateRepository {
-    override fun createTaskState(state: State) {
+class StateRepositoryImpl(
+        private val stateDataSource: StateDataSource
+) : StateRepository {
+    override fun createState(state: State): Result<State> {
         TODO("Not yet implemented")
     }
 
-    override fun editTaskState(state: State) {
+    override fun editState(state: State): Result<State> {
         TODO("Not yet implemented")
     }
 
-    override fun deleteTaskState(state: State) {
+    override fun deleteState(id: UUID): Result<Unit> {
         TODO("Not yet implemented")
     }
+
 }
---
+++ src/test/kotlin/data/repositories/state_repository/StateRepositoryImplTest.kt
@@ -0,0 +1,122 @@
+package data.repositories.state_repository
+
+import data.datasource.state_data_source.StateDataSource
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.example.data.repositories.state_repository.StateRepositoryImpl
+import org.example.models.State
+import org.junit.jupiter.api.BeforeEach
+import java.util.UUID
+import kotlin.test.*
+
+class StateRepositoryImplTest {
+    private lateinit var stateDataSource: StateDataSource
+    private lateinit var stateRepository: StateRepositoryImpl
+
+    @BeforeEach
+    fun setUp() {
+        stateDataSource = mockk(relaxed = true)
+        stateRepository = StateRepositoryImpl(stateDataSource)
+    }
+
+    @Test
+    fun `should create state successfully`() {
+        // When
+        val state = State(id = UUID.randomUUID(), name = "TODO")
+
+        // Given
+        stateRepository.createState(state)
+
+        // Then
+        verify(exactly = 1) { stateDataSource.createState(state) }
+    }
+
+    @Test
+    fun `should fail to create state and return failure result`() {
+        // When
+        val state = State(UUID.randomUUID(), "ERROR")
+        val exception = RuntimeException("Create failed")
+
+        // Given
+        every { stateDataSource.createState(state) } throws exception
+
+        // Then
+        val result = stateRepository.createState(state)
+
+        assertTrue(result.isFailure)
+        assertEquals(exception, result.exceptionOrNull())
+    }
+
+   @Test
+    fun `should fail to create state when name is blank and return failure result`() {
+        // When
+        val state = State(UUID.randomUUID(), "")
+        val exception = RuntimeException("Create failed! : name is Blank !!")
+
+       // Given
+        every { stateDataSource.createState(state) } throws exception
+
+       // Then
+        val result = stateRepository.createState(state)
+
+        assertTrue(result.isFailure)
+        assertEquals(exception, result.exceptionOrNull())
+    }
+
+    @Test
+    fun `should edit state successfully`() {
+        // When
+        val state = State(UUID.randomUUID(), "IN_PROGRESS")
+
+        // Given
+        stateRepository.editState(state)
+
+        // Then
+        verify(exactly = 1) { stateDataSource.editState(state) }
+    }
+
+    @Test
+    fun `should fail to edit state when name is blank and return failure result`() {
+        // When
+        val state = State(UUID.randomUUID(), "")
+        val exception = RuntimeException("Edit failed : name is Blank !!")
+
+        // Given
+        every { stateDataSource.editState(state) } throws exception
+
+        // Then
+        val result = stateRepository.editState(state)
+
+        assertTrue(result.isFailure)
+        assertEquals(exception, result.exceptionOrNull())
+    }
+
+    @Test
+    fun `should delete state successfully`() {
+        // When
+        val id = UUID.randomUUID()
+
+        // Given
+        stateRepository.deleteState(id)
+
+        // Then
+        verify(exactly = 1) { stateDataSource.deleteState(id) }
+    }
+
+    @Test
+    fun `should fail to delete state and return failure result`() {
+        // When
+        val id = UUID.randomUUID()
+        val exception = RuntimeException("Delete failed")
+
+        // Given
+        every { stateDataSource.deleteState(id) } throws exception
+
+        // Then
+        val result = stateRepository.deleteState(id)
+
+        assertTrue(result.isFailure)
+        assertEquals(exception, result.exceptionOrNull())
+    }
+}
