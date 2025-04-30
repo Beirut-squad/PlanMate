c028e79ff701d365e01982006c84065e4fe740f9
Ismail Elkhalili <ismail.elkalili@gmail.com>
2025-04-30 12:55:40 +0300

    fix : given list of Users when writeToFile is called then file shoud be created

--- src/main/kotlin/data/csv/user_csv/UserCsvWriter.kt
+++ src/main/kotlin/data/csv/user_csv/UserCsvWriter.kt
@@ -2,10 +2,26 @@ package org.example.data.csv.user_csv
 
 import org.example.data.csv.CsvWriter
 import org.example.models.User
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
 
 class UserCsvWriter: CsvWriter<User> {
     override fun writeToFile(items: List<User>, filePath: String): Result<Unit> {
-        TODO("Not yet implemented")
+        return try {
+            val file = File(filePath)
+            val writer = BufferedWriter(FileWriter(file))
+
+            writer.write("id,name,password,email,role,isDeleted\n")
+
+            for (user in items){
+                writer.write("${user.id},${user.name},${user.password},${user.role},${user.isDeleted}")
+            }
+            writer.close()
+            Result.success(Unit)
+        }catch (e :Exception){
+            Result.failure(e)
+        }
     }
 
 }
