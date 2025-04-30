dc0f97f7fef71cc63704b9ac60009343381c9628
YaseenOmar <yz456359@gmail.com>
2025-04-30 13:33:51 +0300

    add test cases to StateRepositoryImpl

---
+++ src/test/kotlin/data/repositories/state_repository/StateRepositoryImplTest.kt
@@ -0,0 +1,102 @@
+package data.repositories.state_repository
+
+import data.datasource.state_data_source.StateDataSource
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.example.data.repositories.state_repository.StateRepositoryImpl
+import org.example.models.State
+import org.junit.jupiter.api.BeforeEach
+import java.util.UUID
+import kotlin.test.*
+
+class StateRepositoryImplTest {
+    private lateinit var stateDataSource: StateDataSource
+    private lateinit var stateRepository: StateRepositoryImpl
+
+    @BeforeEach
+    fun setUp() {
+        stateDataSource = mockk(relaxed = true)
+        stateRepository = StateRepositoryImpl(stateDataSource)
+    }
+
+    @Test
+    fun `should create state successfully`() {
+        // When
+        val state = State(id = UUID.randomUUID(), name = "TODO")
+
+        // Given
+        stateRepository.createState(state)
+
+        // Then
+        verify(exactly = 1) { stateDataSource.createState(state) }
+    }
+
+    @Test
+    fun `should fail to create state when name is blank and return failure result`() {
+        // When
+        val state = State(UUID.randomUUID(), "")
+        val exceptionMessage = "Create failed : name is Blank !!"
+
+
+        // Then
+        val result = stateRepository.createState(state)
+
+        assertTrue(result.isFailure)
+        assertEquals(exceptionMessage, result.exceptionOrNull()?.message)
+    }
+
+    @Test
+    fun `should edit state successfully`() {
+        // When
+        val state = State(UUID.randomUUID(), "IN_PROGRESS")
+
+        // Given
+        stateRepository.editState(state)
+
+        // Then
+        verify(exactly = 1) { stateDataSource.editState(state) }
+    }
+
+    @Test
+    fun `should fail to edit state when name is blank and return failure result`() {
+        // Given
+        val state = State(UUID.randomUUID(), "")
+        val exceptionMessage = "Edit failed : name is Blank !!"
+
+        // When
+        val result = stateRepository.editState(state)
+
+        // Then
+        assertTrue(result.isFailure)
+        assertEquals(exceptionMessage, result.exceptionOrNull()?.message)
+    }
+
+    @Test
+    fun `should delete state successfully`() {
+        // When
+        val id = UUID.randomUUID()
+
+        // Given
+        stateRepository.deleteState(id)
+
+        // Then
+        verify(exactly = 1) { stateDataSource.deleteState(id) }
+    }
+
+    @Test
+    fun `should fail to delete state and return failure result`() {
+        // Given
+        val id = UUID.randomUUID()
+        val exceptionMessage = RuntimeException("Delete failed")
+        
+        // When
+        every { stateDataSource.deleteState(id) } throws exceptionMessage
+
+        // Then
+        val result = stateRepository.deleteState(id)
+        assertTrue(result.isFailure)
+        assertEquals(exceptionMessage.message, result.exceptionOrNull()?.message)
+    }
+
+}
