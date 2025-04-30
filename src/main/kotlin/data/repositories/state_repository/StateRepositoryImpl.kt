e75ba465d6dafef313db62d938f5a04c9ae7896b
YaseenOmar <yz456359@gmail.com>
2025-04-30 14:22:42 +0300

    add implementation to StateRepositoryImpl  - convert all test cases from red to green

--- src/main/kotlin/data/repositories/state_repository/StateRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/state_repository/StateRepositoryImpl.kt
@@ -10,15 +10,28 @@ class StateRepositoryImpl(
         private val stateDataSource: StateDataSource
 ) : StateRepository {
     override fun createState(state: State): Result<State> {
-        TODO("Not yet implemented")
+        return if (state.name.isBlank()) {
+            Result.failure(RuntimeException("Create failed : name is Blank !!"))
+        }else{
+            stateDataSource.createState(state)
+        }
     }
 
     override fun editState(state: State): Result<State> {
-        TODO("Not yet implemented")
+       return if (state.name.isBlank()){
+           Result.failure(RuntimeException("Edit failed : name is Blank !!"))
+       }
+        else{
+            stateDataSource.editState(state)
+       }
     }
 
     override fun deleteState(id: UUID): Result<Unit> {
-        TODO("Not yet implemented")
+        return try {
+            stateDataSource.deleteState(id)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 
 }
