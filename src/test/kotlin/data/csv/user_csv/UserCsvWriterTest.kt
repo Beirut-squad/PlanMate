42cc2499c21887dda1d0e07001218cf4956a9d9c
Ismail Elkhalili <ismail.elkalili@gmail.com>
2025-04-30 14:51:11 +0300

    test : given list of Users when writeToFile is called then file shoud be created and sotre users

--- src/test/kotlin/data/csv/user_csv/UserCsvWriterTest.kt
+++ src/test/kotlin/data/csv/user_csv/UserCsvWriterTest.kt
@@ -4,6 +4,7 @@ import creator_helper.createCsvLineForUser
 import creator_helper.createUserForCsvWriter
 import creator_helper.createUserHelper
 import org.example.data.csv.user_csv.UserCsvWriter
+import org.example.models.Role
 import org.example.models.User
 import org.junit.jupiter.api.Assertions.*
 import java.io.File
@@ -32,5 +33,55 @@ class UserCsvWriterTest{
 
   //then
   assertTrue(File(filePath).exists())
+
  }
+
+ @Test
+ fun `given empty list of Users when writeToFile is called then file shoud contain only header`(){
+     //given
+     val users = emptyList<User>()
+
+     //then
+     val result = userCsvWriter.writeToFile(users,filePath)
+     val file = File(filePath)
+
+     //then
+     val content = file.readText()
+     assertTrue(file.exists())
+     assertTrue(content.contains("id,name,password,email,role,isDeleted"))
+     assertTrue(content.lines().size == 2)
+     assertTrue(content.lines()[1].isBlank())
+ }
+
+ @Test
+ fun `given list of Users when writeToFile is called then file shoud be created and sotre users`(){
+     //given
+     val user  = createUserForCsvWriter()
+     val mateUser  = createUserForCsvWriter(role = Role.MATE.toString())
+     val deletedUser   = createUserForCsvWriter(isDeleted = true)
+     val activeUser   = createUserForCsvWriter()
+     val testUser   = createUserForCsvWriter(name = "testUser")
+
+     val listOfUsers = listOf(user, mateUser , deletedUser  , testUser , activeUser)
+     val filePath = "test_users.csv"
+
+     //when
+     val result = userCsvWriter.writeToFile(listOfUsers , filePath)
+
+     //then
+    assertTrue(result.isSuccess)
+     val file = File(filePath)
+    assertTrue(file.exists())
+     val lines = file.readLines()
+     assertTrue(lines.size == 6)
+     assertTrue(lines[0].contains("id,name,password,email,role,isDeleted"))
+     assertTrue(lines[1].contains("Ismail"))
+     assertTrue(lines[2].contains(Role.MATE.toString()))
+     assertTrue(lines[3].contains("true"))
+
+     file.delete()
+
+ }
+
+
  }
