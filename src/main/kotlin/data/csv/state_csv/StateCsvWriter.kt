052847645480fadb4040b552dbdad9a8163b331c
Ismail Elkhalili <ismail.elkalili@gmail.com>
2025-05-01 08:26:29 +0300

    test : create StateCsvWriterTest calss - implement CsvWriter interface for State

--- src/main/kotlin/data/csv/state_csv/StateCsvWriter.kt
+++ src/main/kotlin/data/csv/state_csv/StateCsvWriter.kt
@@ -1,4 +1,10 @@
 package org.example.data.csv.state_csv
 
-class StateCsvWriter {
+import org.example.data.csv.CsvWriter
+import org.example.models.State
+
+class StateCsvWriter: CsvWriter<State> {
+    override fun writeToFile(items: List<State>, filePath: String): Result<Unit> {
+        TODO("Not yet implemented")
+    }
 }
