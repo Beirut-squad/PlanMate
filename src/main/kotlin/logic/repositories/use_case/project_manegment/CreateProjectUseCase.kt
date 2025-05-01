c293a7af91d19f95df6c1ea19cbe6f94824567fb
Bassant Mohammed <bassant.22230@gmail.com>
2025-05-01 22:59:45 +0300

    update create project use case and test due to develop updating

---
+++ src/main/kotlin/logic/repositories/use_case/project_manegment/CreateProjectUseCase.kt
@@ -0,0 +1,37 @@
+package org.example.logic.repositories.use_case.project_manegment
+
+import org.example.logic.exceptions.BlankFieldsException
+import org.example.logic.exceptions.ProjectNotCreatedException
+import org.example.logic.repositories.project_repository.ProjectRepository
+import org.example.logic.repositories.use_case.log.CreateProjectLogUseCase
+import org.example.models.Project
+import org.example.models.State
+import java.time.LocalDateTime
+import java.util.*
+
+class CreateProjectUseCase(
+    private val projectRepository: ProjectRepository,
+    private val logUseCase: CreateProjectLogUseCase
+) {
+    fun createProject(creatorUserID: UUID, name: String, description: String, stateNames: List<String>) {
+        if (name.isBlank() || description.isBlank()) {
+            throw BlankFieldsException("must not be blank")
+        } else {
+            val project = buildProject(creatorUserID,name, description, stateNames)
+            logUseCase.createProjectLog(creatorUserID, previousProject = null, currentProject = project)
+            projectRepository.createProject(project)
+        }
+    }
+
+    private fun buildProject(creatorUserID: UUID, name: String, description: String, stateNames: List<String>): Project {
+        return Project(
+            id = UUID.randomUUID(),
+            name = name,
+            description = description,
+            state = stateNames.map { State(id = UUID.randomUUID(), name = it) },
+            creatorUserID = creatorUserID,
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now()
+        )
+    }
+}
--- src/test/kotlin/logic/use_case/project_manegement/CreateProjectUseCaseTest.kt
+++ src/test/kotlin/logic/use_case/project_manegement/CreateProjectUseCaseTest.kt
@@ -1,46 +1,126 @@
 package logic.use_case.project_manegement
 
+import creator_helper.createProjectHelper
 import io.mockk.mockk
 import io.mockk.verify
+import org.example.logic.exceptions.BlankFieldsException
 import org.example.logic.repositories.project_repository.ProjectRepository
-import org.example.logic.use_case.project_manegement.CreateProjectUseCase
+import org.example.logic.repositories.use_case.log.CreateProjectLogUseCase
+import org.example.logic.repositories.use_case.project_manegment.CreateProjectUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import sun.net.util.IPAddressUtil.match
 import java.util.UUID
+import kotlin.test.assertEquals
 
 class CreateProjectUseCaseTest {
     private val projectRepository: ProjectRepository = mockk(relaxed = true)
-    private lateinit var createProjectUseCase: CreateProjectUseCase
+    private val logUseCase: CreateProjectLogUseCase = mockk(relaxed = true)
+    private var createProjectUseCase: CreateProjectUseCase = mockk()
+    private val creatorUserID = UUID.randomUUID()
+
+
     @BeforeEach
     fun setup() {
-        createProjectUseCase = CreateProjectUseCase(projectRepository)
+        createProjectUseCase = CreateProjectUseCase(projectRepository,logUseCase)
     }
+
     @Test
     fun `should create project successfully when call createProject function`() {
         // Given
-        val creatorUserID : UUID = UUID.randomUUID()
-        val name = "Test Project"
+        val name = "x"
         val description = "Test Description"
         val stateNames = listOf("Todo", "In Progress", "Done")
 
         // When
         createProjectUseCase.createProject(
+            creatorUserID = creatorUserID,
             name = name,
             description = description,
             stateNames = stateNames,
-            creatorUserID = creatorUserID,
         )
 
         // Then
         verify(exactly = 1) {
-            projectRepository.createProject(match { project ->
-                project.name == name &&
-                        project.description == description &&
-                        project.creatorUserID == creatorUserID &&
-                        project.state.size == stateNames.size &&
-                        project.state.map { it.name } == stateNames
-            })
+            projectRepository.createProject(
+                match { project ->
+                    project.name == name &&
+                            project.description == description &&
+                            project.creatorUserID == creatorUserID &&
+                            project.state.map { it.name } == stateNames
+                }
+            )
         }
     }
-}
 
+    @Test
+    fun `should not create project successfully when name is blank`() {
+        // Given
+        val name = ""
+        val description = "Test Description"
+        val stateNames = listOf("Todo", "In Progress", "Done")
+
+        // When
+        val exception = assertThrows<BlankFieldsException> {
+            createProjectUseCase.createProject(
+                creatorUserID = creatorUserID,
+                name = name,
+                description = description,
+                stateNames = stateNames,
+            )
+        }
+
+        // Then
+        assertEquals("must not be blank", exception.message)
+        verify(exactly = 0) {projectRepository.createProject(any())  }
+        verify(exactly = 0) { logUseCase.createProjectLog(creatorUserID,null,any()) }
+    }
+
+    @Test
+    fun `should not create project successfully when description is blank`() {
+        // Given
+        val name = "Test Description"
+        val description = ""
+        val stateNames = listOf("Todo", "In Progress", "Done")
+
+        // When
+        val exception = assertThrows<BlankFieldsException> {
+            createProjectUseCase.createProject(
+                creatorUserID = creatorUserID,
+                name = name,
+                description = description,
+                stateNames = stateNames,
+            )
+        }
+
+        // Then
+        assertEquals("must not be blank", exception.message)
+        verify(exactly = 0) {projectRepository.createProject(any())  }
+        verify(exactly = 0) { logUseCase.createProjectLog(creatorUserID,null,any()) }
+    }
+
+    @Test
+    fun `should not create project successfully when name and description are blank`() {
+        // Given
+        val name = ""
+        val description = ""
+        val stateNames = listOf("Todo", "In Progress", "Done")
+
+        // When
+        val exception = assertThrows<BlankFieldsException> {
+            createProjectUseCase.createProject(
+                creatorUserID = creatorUserID,
+                name = name,
+                description = description,
+                stateNames = stateNames,
+            )
+        }
+
+        // Then
+        assertEquals("must not be blank", exception.message)
+        verify(exactly = 0) {projectRepository.createProject(any())  }
+        verify(exactly = 0) { logUseCase.createProjectLog(creatorUserID,null,any()) }
+    }
+
+}
