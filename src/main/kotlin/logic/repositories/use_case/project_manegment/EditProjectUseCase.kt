d446327e4893fc6dda0413491d6ccefc8e09ee18
Bassant Mohammed <bassant.22230@gmail.com>
2025-05-01 23:00:15 +0300

    update edit project use case and test due to develop updating

---
+++ src/main/kotlin/logic/repositories/use_case/project_manegment/EditProjectUseCase.kt
@@ -0,0 +1,37 @@
+package org.example.logic.repositories.use_case.project_manegment
+
+import org.example.logic.exceptions.BlankFieldsException
+import org.example.logic.repositories.project_repository.ProjectRepository
+import org.example.logic.repositories.use_case.log.CreateProjectLogUseCase
+import org.example.models.Project
+import java.time.LocalDateTime
+import java.util.*
+
+
+class EditProjectUseCase(
+    private val projectRepository: ProjectRepository,
+    private val logUseCase: CreateProjectLogUseCase
+) {
+    fun editProject(
+        newProject: Project,
+        newName: String?,
+        newDescription: String?,
+        creatorUserID: UUID
+    ) {
+        if (newName == null && newDescription == null) {
+            throw BlankFieldsException("unacceptable blank")
+        } else {
+
+            newProject.name = if (newName.isNullOrBlank()) newProject.name else newName
+            newProject.description = if (newDescription.isNullOrBlank()) newProject.description else newDescription
+            newProject.updatedAt = LocalDateTime.now()
+
+            logUseCase.createProjectLog(
+                previousProject = newProject,
+                currentProject = newProject,
+                userId = creatorUserID
+            )
+            projectRepository.editProject(newProject)
+        }
+    }
+}
--- src/test/kotlin/logic/use_case/project_manegement/EditProjectUseCaseTest.kt
+++ src/test/kotlin/logic/use_case/project_manegement/EditProjectUseCaseTest.kt
@@ -1,69 +1,130 @@
 package logic.use_case.project_manegement
 
 import creator_helper.createProjectHelper
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.example.logic.exceptions.BlankFieldsException
 import org.example.logic.repositories.project_repository.ProjectRepository
-import org.example.logic.use_case.project_manegement.EditProjectUseCase
+import org.example.logic.repositories.use_case.log.CreateProjectLogUseCase
+import org.example.logic.repositories.use_case.project_manegment.EditProjectUseCase
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.util.UUID
 
 class EditProjectUseCaseTest {
     private val projectRepository: ProjectRepository = mockk(relaxed = true)
-    private lateinit var editProjectUseCase: EditProjectUseCase
+    private val logUseCase: CreateProjectLogUseCase = mockk(relaxed = true)
+    private var editProjectUseCase: EditProjectUseCase = mockk(relaxed = true)
+    private val creatorUserID: UUID = UUID.randomUUID()
 
     @BeforeEach
     fun setup() {
-        editProjectUseCase = EditProjectUseCase(projectRepository)
+        editProjectUseCase = EditProjectUseCase(projectRepository, logUseCase)
     }
 
     @Test
-    fun `should edit project successfully when call edit Project Name function`() {
-        //Given
+    fun `should edit project name only when newName is provided and newDescription is null`() {
+        // Given
         val project = createProjectHelper()
-        val userId = UUID.randomUUID()
-        editProjectUseCase.editProjectName(
-            project = project,
-            newName = "Bassant",
-            creatorUserID = userId
+        val newName = "xyz"
+        val newDescription = "zyx"
+
+        //every { projectRepository.editProject(project) } returns Result.success(project)
+
+        // When
+        editProjectUseCase.editProject(
+            newProject = project,
+            newName = newName,
+            newDescription = newDescription,
+            creatorUserID = creatorUserID
         )
-        //When
-        projectRepository.editProject(project)
+
         //Then
-        verify(exactly = 1) { projectRepository.editProject(project) }
+        project.name = newName
+        project.description = newDescription
+        verify(exactly = 1) {
+            projectRepository.editProject(project)
+        }
+
+        verify(exactly = 1) {
+            logUseCase.createProjectLog(
+                previousProject = match { it.name == newName },
+                currentProject = match { it.name == newName },
+                userId = creatorUserID
+            )
+        }
+
+        assertEquals(newName, project.name)
+        assertEquals(newDescription, project.description)
     }
 
     @Test
-    fun `should edit project successfully when call edit project description function`() {
-        //Given
+    fun `should throw exception when both newName and newDescription are null`() {
+        // Given
         val project = createProjectHelper()
-        val userId = UUID.randomUUID()
-        editProjectUseCase.editProjectDescription(
-            project = project,
-            newDescription = "Bassant",
-            creatorUserID = userId
-        )
-        //When
-        projectRepository.editProject(project)
-        //Then
-        verify(exactly = 1) { projectRepository.editProject(project) }
+
+        // When/Then
+        assertThrows<BlankFieldsException> {
+            editProjectUseCase.editProject(
+                newProject = project,
+                newName = null,
+                newDescription = null,
+                creatorUserID = creatorUserID
+            )
+        }
     }
 
     @Test
-    fun `should edit project successfully when call edit project name and description function`() {
-        //Given
+    fun `should throw exception when both newName and newDescription are blank`() {
+        // Given
         val project = createProjectHelper()
-        val userId = UUID.randomUUID()
-        editProjectUseCase.editProjectNameAndDescription(
-            project = project,
-            newName = "",
-            newDescription = "Bassant",
-            creatorUserID = userId
-        )
-        //When
-        projectRepository.editProject(project)
-        //Then
-        verify(exactly = 1) { projectRepository.editProject(project) }
+
+         //When & Then
+        verify (exactly = 0) {
+            editProjectUseCase.editProject(
+                newProject = project,
+                newName = "",
+                newDescription = "",
+                creatorUserID = creatorUserID
+            )
+        }
+
     }
+
+    @Test
+    fun `should throw exception when newName is null`() {
+        // Given
+        val project = createProjectHelper()
+
+        // When/Then
+        verify (exactly = 0){
+            editProjectUseCase.editProject(
+                newProject = project,
+                newName = null,
+                newDescription = "x",
+                creatorUserID = creatorUserID
+            )
+        }
+    }
+
+    @Test
+    fun `should throw exception when newDescription is null`() {
+        // Given
+        val project = createProjectHelper()
+
+        // When/Then
+        verify (exactly = 0) {
+            editProjectUseCase.editProject(
+                newProject = project,
+                newName = "x",
+                newDescription = null,
+                creatorUserID = creatorUserID
+            )
+        }
+    }
+
 }
