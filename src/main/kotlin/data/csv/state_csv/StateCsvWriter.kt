13690e7895c84ef8caaa42dd076cd202b496c4b8
Ismail Elkhalili <ismail.elkalili@gmail.com>
2025-05-01 08:54:50 +0300

    fix(state-csv) : fix test to verify file created after writer

--- src/main/kotlin/data/csv/state_csv/StateCsvWriter.kt
+++ src/main/kotlin/data/csv/state_csv/StateCsvWriter.kt
@@ -2,9 +2,21 @@ package org.example.data.csv.state_csv
 
 import org.example.data.csv.CsvWriter
 import org.example.models.State
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
 
 class StateCsvWriter: CsvWriter<State> {
     override fun writeToFile(items: List<State>, filePath: String): Result<Unit> {
-        TODO("Not yet implemented")
+
+     return try{
+         val file = File(filePath)
+         val writer = BufferedWriter(FileWriter(file))
+         return Result.success(Unit)
+
+     }catch (e:Exception){
+         return Result.failure(e)
+     }
+
     }
 }
