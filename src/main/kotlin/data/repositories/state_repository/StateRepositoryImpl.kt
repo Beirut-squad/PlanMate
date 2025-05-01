fc198220242311a5382e30ab2668a2c4ed7e9671
YaseenOmar <yz456359@gmail.com>
2025-05-01 18:08:47 +0300

    add test

--- src/main/kotlin/data/repositories/state_repository/StateRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/state_repository/StateRepositoryImpl.kt
@@ -1,19 +1,37 @@
 package org.example.data.repositories.state_repository
 
+import data.datasource.state_data_source.StateDataSource
 import org.example.logic.repositories.state_repository.StateRepository
 import org.example.models.State
+import java.util.UUID
 
 
-class StateRepositoryImpl : StateRepository {
-    override fun createTaskState(state: State) {
-        TODO("Not yet implemented")
+class StateRepositoryImpl(
+    private val stateDataSource: StateDataSource
+) : StateRepository {
+    override fun createState(state: State): Result<State> {
+        return if (state.name.isBlank()) {
+            Result.failure(RuntimeException("Create failed : name is Blank !!"))
+        }else{
+            stateDataSource.createState(state)
+        }
     }
 
-    override fun editTaskState(state: State) {
-        TODO("Not yet implemented")
+    override fun editState(state: State): Result<State> {
+        return if (state.name.isBlank()){
+            Result.failure(RuntimeException("Edit failed : name is Blank !!"))
+        }
+        else{
+            stateDataSource.editState(state)
+        }
     }
 
-    override fun deleteTaskState(state: State) {
-        TODO("Not yet implemented")
+    override fun deleteState(id: UUID): Result<Unit> {
+        return try {
+            stateDataSource.deleteState(id)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
+
 }
---
+++ src/test/kotlin/data/repositories/state_repository/StateRepositoryImplTest.kt
@@ -0,0 +1,15 @@
+package data.repositories.state_repository
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class StateRepositoryImplTest {
+    @Test
+    fun test() {
+        // Given
+
+        // Then
+        assertEquals(8, 3)
+    }
+    }
