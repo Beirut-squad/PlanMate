d48eb75dc677ba0cc0f65b68812fe2cddfbbecc4
Ismail Elkhalili <ismail.elkalili@gmail.com>
2025-05-01 12:41:19 +0300

    create ProjectCsvWriterTest.kt

--- src/main/kotlin/data/csv/project_csv/ProjectCsvWriter.kt
+++ src/main/kotlin/data/csv/project_csv/ProjectCsvWriter.kt
@@ -1,4 +1,10 @@
 package org.example.data.csv.project_csv_parser
 
-class ProjectCsvWriter {
+import org.example.data.csv.CsvWriter
+import org.example.models.Project
+
+class ProjectCsvWriter: CsvWriter<Project> {
+    override fun writeToFile(items: List<Project>, filePath: String): Result<Unit> {
+        TODO("Not yet implemented")
+    }
 }
---
+++ src/test/kotlin/data/csv/project_csv/ProjectCsvWriterTest.kt
@@ -0,0 +1,6 @@
+package data.csv.project_csv
+
+import org.junit.jupiter.api.Assertions.*
+ class ProjectCsvWriterTest{
+
+ }
