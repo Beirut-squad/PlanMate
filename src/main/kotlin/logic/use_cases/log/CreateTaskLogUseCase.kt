3fb54c0b2e4242082d7196fb6b7bdf6d3427c2d5
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-05-01 17:13:03 +0300

    add implementation for create task log use case - green phase

--- src/main/kotlin/logic/use_cases/log/CreateTaskLogUseCase.kt
+++ src/main/kotlin/logic/use_cases/log/CreateTaskLogUseCase.kt
@@ -2,12 +2,32 @@ package logic.use_cases.log
 
 import org.example.logic.repositories.log_repository.LogRepository
 import org.example.models.Task
+import org.example.models.TaskLog
+import java.time.LocalDateTime
 import java.util.UUID
 
 class CreateTaskLogUseCase(
     private val logRepository: LogRepository
 ) {
     fun createTaskLog(userId: UUID, previousTask: Task?, currentTask: Task?): Result<Unit> {
-        TODO()
+        if (previousTask == null && currentTask == null) return Result.failure(
+            IllegalArgumentException("Both previousTask and currentTask cannot be null")
+        )
+
+        val entityId = listOfNotNull(currentTask?.id, previousTask?.id).first()
+
+        return logRepository.saveTaskLog(
+            TaskLog(
+                id = UUID.randomUUID(),
+                userId = userId,
+                entityId = entityId,
+                previousEntity = previousTask,
+                currentEntity = currentTask,
+                createdAt = LocalDateTime.now()
+            )
+        ).fold(
+            onSuccess = { Result.success(Unit) },
+            onFailure = { Result.failure(it) }
+        )
     }
 }
