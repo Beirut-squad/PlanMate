b904b11c1c81d9c962a872d52d76af89488dd616
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-05-02 04:29:54 +0300

    implement fetching task logs by task id

--- src/main/kotlin/logic/use_cases/log/GetTaskLogsByTaskIdUseCase.kt
+++ src/main/kotlin/logic/use_cases/log/GetTaskLogsByTaskIdUseCase.kt
@@ -8,6 +8,6 @@ class GetTaskLogsByTaskIdUseCase(
     private val logRepository: LogRepository
 ) {
     fun getTaskLogsByTaskId(taskId: UUID): Result<List<TaskLog>> {
-        TODO()
+        return logRepository.getTaskLogs(taskId)
     }
 }
