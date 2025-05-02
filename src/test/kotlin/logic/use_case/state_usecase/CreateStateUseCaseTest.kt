87aaf237d27cf8617ae46a093d24d7f572508f54
Bassant Mohammed <bassant.22230@gmail.com>
2025-05-03 00:35:00 +0300

    - refactor parameter in create state from state to name of state - update test cases due to this change still fully covered

--- src/test/kotlin/logic/use_case/state_usecase/CreateStateUseCaseTest.kt
+++ src/test/kotlin/logic/use_case/state_usecase/CreateStateUseCaseTest.kt
@@ -2,11 +2,15 @@ package logic.use_case.state_usecase
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import io.mockk.verify
 import junit.framework.TestCase.assertTrue
 import org.example.logic.repositories.state_repository.StateRepository
 import org.example.logic.use_case.state_usecase.CreateStateUseCase
 import org.example.models.State
+import org.example.models.Task
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import java.util.UUID
 import kotlin.test.Test
@@ -15,7 +19,7 @@ import kotlin.test.assertEquals
 class CreateStateUseCaseTest {
     private lateinit var stateRepository: StateRepository
     private lateinit var createStateUseCase: CreateStateUseCase
-
+    private val id = UUID.randomUUID()
 
     @BeforeEach
     fun setup() {
@@ -24,32 +28,13 @@ class CreateStateUseCaseTest {
     }
 
     @Test
-    fun `should create state successfully when name is valid`() {
-        // When
-        val id = UUID.randomUUID()
-        val stateName = "TODO"
-        val state = State(id, stateName)
-
-        // Given
-        every { stateRepository.createState(state) } returns Result.success(State(id, stateName))
-
-        // Then
-        val result = createStateUseCase.createState(state)
-        verify(exactly = 1) { stateRepository.createState(state) }
-        assertTrue(result.isSuccess)
-        assertEquals(state, result.getOrNull())
-    }
-
-
-    @Test
     fun `should return failure when name is blank`() {
         // When
-        val id = UUID.randomUUID()
         val blankName = ""
         val state = State(id, blankName)
 
         // Given
-        val result = createStateUseCase.createState(state)
+        val result = createStateUseCase.createState(blankName)
 
         // Then
         assertTrue(result.isFailure)
@@ -60,14 +45,13 @@ class CreateStateUseCaseTest {
     @Test
     fun `should return failure when repository fails`() {
         // When
-        val id = UUID.randomUUID()
         val name = "TODO"
         val state = State(id, name)
         val exception = RuntimeException("Repo failed")
 
         // Given
         every { stateRepository.createState(any()) } returns Result.failure(exception)
-        val result = createStateUseCase.createState(state)
+        val result = createStateUseCase.createState(name)
 
         // Then
         assertTrue(result.isFailure)
