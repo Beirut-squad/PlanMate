749ba86b3e74f0c6d4db550f51a966e8cbed3336
Zyad <zyadhammad531@gmail.com>
2025-05-03 02:01:12 +0300

    import modules

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 package org.example
 
+import org.example.di.dataModule
+import org.example.di.logicModule
+import org.example.di.uiModule
+import org.koin.core.context.startKoin
+
 fun main() {
     startKoin {
         modules(
