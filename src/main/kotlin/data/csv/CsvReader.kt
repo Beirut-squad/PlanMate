415759575cb9a759f0dd5abf05957e94015ffc4b
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-05-02 20:05:27 +0300

    add log data source implementation

--- src/main/kotlin/data/csv/CsvReader.kt
+++ src/main/kotlin/data/csv/CsvReader.kt
@@ -1,14 +1,18 @@
 package org.example.data.csv
 
 import CsvParser
+import data.csv.FileName
 import java.io.File
 
 class CsvReader<T>(private val parser: CsvParser<T>){
-
     fun read(fileName: String): List<T>{
-        val csvLines = File(fileName).readLines()
-        return csvLines.mapNotNull { line ->
-            parser.parseLine(line)
-        }
+        if (fileName !in FileName.allFiles)
+            throw Exception("File $fileName cannot be found in file names")
+        val filePath = "src/main/kotlin/$fileName"
+        val file = File(filePath)
+        if (!file.exists())
+            throw Exception("File $fileName cannot be found in $filePath ")
+        val csvLines = file.readLines()
+        return  parser.parseFile(csvLines)
     }
 }
