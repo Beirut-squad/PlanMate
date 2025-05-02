31c5f1b3b60f25737c5b07dbf67c807caeefcdd8
yousef_osama11 <oyousef709@gmail.com>
2025-05-02 22:55:24 +0300

    refactor StateCsvWriter

--- src/main/kotlin/data/csv/state_csv/StateCsvWriter.kt
+++ src/main/kotlin/data/csv/state_csv/StateCsvWriter.kt
@@ -3,6 +3,7 @@ package org.example.data.csv.state_csv
 import org.example.data.csv.CsvWriter
 import org.example.data.csv.isValidFileName
 import org.example.models.State
+import org.example.models.TaskLog
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
@@ -11,35 +12,27 @@ import java.util.UUID
 
 class StateCsvWriter: CsvWriter<State> {
     override fun writeToFile(items: List<State>, filePath: String): Result<Unit> {
-     return runCatching {
-             val file = File(filePath)
-
-             if (!isValidFileName(file.name)){
-                 throw IllegalArgumentException("Error: Invalid file name")
-             }
-
-             val writer = BufferedWriter(FileWriter(file))
-             if (file.length() == 0L){
-                 writer.write("[id,name]\n")
-             }
-
-             for (state in items){
-                 if (isValidState(state))
-                    writer.write("[${state.id},${state.name}]\n")
-             }
-             writer.close()
-
-     }.fold(
-         onSuccess = {
-             return Result.success(Unit)
-         },
-         onFailure = {
-             return Result.failure(it)
-         }
-     )
-
+        return runCatching {
+            val file = File(filePath)
+            if (!isValidFileName(file.name))
+                throw IllegalArgumentException("Error: Invalid file name")
+            val writer = BufferedWriter(FileWriter(file))
+            if (file.length() == 0L)
+                writer.write("[id,name]\n")
+            if (items.isNotEmpty())
+                writeState(items, writer)
+            writer.close()
+        }.fold(
+            onSuccess = { return Result.success(Unit) },
+            onFailure = { return Result.failure(it) }
+        )
     }
-
+    private fun writeState(items: List<State>, writer: BufferedWriter) {
+        items.forEach { state ->
+            if (isValidState(state))
+                writer.write("[${state.id},${state.name}]\n")
+        }
+    }
     internal fun isValidState(state: State): Boolean {
         return state.name.isNotBlank() && state.id != UUID(0,0)
     }
