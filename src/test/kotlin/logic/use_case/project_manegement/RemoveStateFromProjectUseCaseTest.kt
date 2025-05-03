d525d8c5a84e723ccee4d88c076e52b1dc6b6f64
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-05-03 12:27:00 +0300

    feat: add logs for state add, edit, and remove actions in project use cases

--- src/test/kotlin/logic/use_case/project_manegement/RemoveStateFromProjectUseCaseTest.kt
+++ src/test/kotlin/logic/use_case/project_manegement/RemoveStateFromProjectUseCaseTest.kt
@@ -2,10 +2,13 @@ package logic.use_case.project_manegement
 
 import creator_helper.createProjectHelper
 import creator_helper.createStateHelper
+import creator_helper.createUserHelper
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import logic.use_cases.log.CreateProjectLogUseCase
 import org.example.constants.StringConstants
+import org.example.logic.exceptions.project_magement_exceptions.BlankFieldsException
 import org.example.logic.exceptions.project_magement_exceptions.NoProjectFoundException
 import org.example.logic.exceptions.project_magement_exceptions.NoStateException
 import org.example.logic.exceptions.project_magement_exceptions.StateHasAssociatedTasksException
@@ -18,40 +21,45 @@ import java.util.*
 
 class RemoveStateFromProjectUseCaseTest {
     private lateinit var repository: ProjectRepository
+    private lateinit var logUseCase: CreateProjectLogUseCase
     private lateinit var useCase: RemoveStateFromProjectUseCase
 
     @BeforeEach
     fun setUp() {
         repository = mockk(relaxed = true)
-        useCase = RemoveStateFromProjectUseCase(repository)
+        logUseCase = mockk(relaxed = true)
+        useCase = RemoveStateFromProjectUseCase(repository, logUseCase)
     }
 
     //region Test cases for removeStateFromProject()
     @Test
     fun `removeStateFromProject should call removeStateFromProject from ProjectRepository exactly once`() {
         // Given
+        val currentUser = createUserHelper()
         val removeState = createStateHelper()
         val project = createProjectHelper(state = listOf(removeState))
-        every { repository.removeStateFromProject(project.id, removeState) } returns Result.success(Unit)
+        every { repository.removeStateFromProject(project.id, removeState) } returns Result.success(project)
 
         // When
-        val result = useCase.removeStateFromProject(project.id, removeState)
+        val result = useCase.removeStateFromProject(currentUser.id,project, removeState)
 
         // Then
         verify(exactly = 1) {
             repository.removeStateFromProject(project.id, removeState)
+            logUseCase.createProjectLog(userId = currentUser.id, previousProject = project, currentProject = result.getOrNull())
         }
     }
 
     @Test
     fun `removeStateFromProject should return success with Unit when state exists and has no tasks`() {
         // Given
+        val currentUser = createUserHelper()
         val removeState = createStateHelper()
         val project = createProjectHelper(state = listOf(removeState))
-        every { repository.removeStateFromProject(project.id, removeState) } returns Result.success(Unit)
+        every { repository.removeStateFromProject(project.id, removeState) } returns Result.success(project)
 
         // When
-        val result = useCase.removeStateFromProject(project.id, removeState)
+        val result = useCase.removeStateFromProject(currentUser.id,project, removeState)
 
         // Then
         assertTrue(result.isSuccess)
@@ -59,8 +67,25 @@ class RemoveStateFromProjectUseCaseTest {
     }
 
     @Test
+    fun `addStateToProject should return failure with BlankFieldsException when user enters empty state name`() {
+        // Given
+        val currentUser = createUserHelper()
+        val project = createProjectHelper()
+        val newState = createStateHelper(name = " ")
+
+        // When
+        val result = useCase.removeStateFromProject(currentUser.id, project, newState)
+
+        // Then
+        kotlin.test.assertTrue(result.isFailure)
+        val exception = result.exceptionOrNull()
+        kotlin.test.assertTrue(exception is BlankFieldsException)
+    }
+
+    @Test
     fun `removeStateFromProject should failure with NoProjectFoundException when project does not exist`() {
         // Given
+        val currentUser = createUserHelper()
         val removeState = createStateHelper()
         val project = createProjectHelper(state = listOf(removeState))
         every { repository.removeStateFromProject(project.id, removeState) } returns Result.failure(
@@ -68,7 +93,7 @@ class RemoveStateFromProjectUseCaseTest {
         )
 
         // When
-        val result = useCase.removeStateFromProject(project.id, removeState)
+        val result = useCase.removeStateFromProject(currentUser.id,project, removeState)
 
         // Then
         assertTrue(result.isFailure)
@@ -80,6 +105,7 @@ class RemoveStateFromProjectUseCaseTest {
     @Test
     fun `removeStateFromProject should failure with NoStateException when state does not exist`() {
         // Given
+        val currentUser = createUserHelper()
         val removeState = createStateHelper(id = UUID.randomUUID(), name = "Undo")
         val project = createProjectHelper()
         every { repository.removeStateFromProject(project.id, removeState) } returns Result.failure(
@@ -87,7 +113,7 @@ class RemoveStateFromProjectUseCaseTest {
         )
 
         // When
-        val result = useCase.removeStateFromProject(project.id, removeState)
+        val result = useCase.removeStateFromProject(currentUser.id,project, removeState)
 
         // Then
         assertTrue(result.isFailure)
@@ -99,6 +125,7 @@ class RemoveStateFromProjectUseCaseTest {
     @Test
     fun `removeStateFromProject should failure with StateHasAssociatedTasksException when state has associated with tasks`() {
         // Given
+        val currentUser = createUserHelper()
         val removeState = createStateHelper()
         val project = createProjectHelper(state = listOf(removeState))
         every { repository.removeStateFromProject(project.id, removeState) } returns Result.failure(
@@ -106,7 +133,7 @@ class RemoveStateFromProjectUseCaseTest {
         )
 
         // When
-        val result = useCase.removeStateFromProject(project.id, removeState)
+        val result = useCase.removeStateFromProject(currentUser.id,project, removeState)
 
         // Then
         assertTrue(result.isFailure)
