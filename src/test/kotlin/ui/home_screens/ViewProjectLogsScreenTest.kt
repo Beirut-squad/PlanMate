59e6f8fcb32420dbae278f64cce8bfe6f1266149
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-05-03 12:13:34 +0300

    remove unused code

--- src/test/kotlin/ui/home_screens/ViewProjectLogsScreenTest.kt
+++ src/test/kotlin/ui/home_screens/ViewProjectLogsScreenTest.kt
@@ -8,7 +8,6 @@ import io.mockk.verify
 import logic.use_cases.log.GetUserProjectLogsUseCase
 import org.example.logic.use_cases.authentication.GetCurrentLoggedInUserUseCase
 import org.example.models.User
-import org.example.ui.UiScreen
 import org.example.ui.home_screens.ViewProjectLogsScreen
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -104,46 +103,4 @@ class ViewProjectLogsScreenTest {
         verify(exactly = 1) { viewer.printInfoLine("No project logs found for the current user.") }
     }
 
-}
-
-class ViewProjectLogsScreen(
-    private val viewer: Viewer,
-    private val getCurrentLoggedInUserUseCase: GetCurrentLoggedInUserUseCase,
-    private val getUserProjectLogsUseCase: GetUserProjectLogsUseCase
-) : UiScreen {
-    override fun show() {
-        val currentUserResult = getCurrentLoggedInUserUseCase.getCurrentUser()
-
-        val user = currentUserResult.getOrNull()
-        if (user == null) {
-            viewer.printError("No user logged in")
-            return
-        }
-
-        val userLogsResult = getUserProjectLogsUseCase.getUserProjectLogs(user.id)
-
-        userLogsResult.fold(
-            onSuccess = { logs ->
-                if (logs.isNotEmpty()) {
-                    viewer.printTitle("Project Logs for User: ${user.name}")
-                    logs.forEachIndexed { index, log ->
-                        viewer.printInfoLine(
-                            """
-                        ${index + 1}.
-                        - Change made by: ${log.userId}
-                        - Previous: ${log.previousEntity}
-                        - Current: ${log.currentEntity}
-                        - Timestamp: ${log.createdAt}
-                    """.trimIndent()
-                        )
-                    }
-                } else {
-                    viewer.printInfoLine("No project logs found for the current user.")
-                }
-            },
-            onFailure = {
-                viewer.printError("Failed to retrieve project logs: ${it.message}")
-            }
-        )
-    }
 }
