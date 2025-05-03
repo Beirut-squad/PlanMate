69a0ff9869abf6e9005968ff0b4c81ef1b4d7430
Bassant Mohammed <bassant.22230@gmail.com>
2025-05-03 04:58:21 +0300

    refactor - separate exceptions files

---
+++ src/main/kotlin/logic/exceptions/log_exceptions/log_exceptions.kt
@@ -0,0 +1,4 @@
+package org.example.logic.exceptions.log_exceptions
+
+class NoProjectLogsFoundException : Exception("No project logs found")
+class NoTaskLogsFoundException : Exception("No task logs found")
