9c233785fab0f4fcd495de52cf648d5068bb5d9a
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-05-05 19:31:42 +0300

    enhanced view state selected ui for mate

--- src/main/kotlin/ui/common/screens/ViewStateSelectedForProject.kt
+++ src/main/kotlin/ui/common/screens/ViewStateSelectedForProject.kt
@@ -2,21 +2,43 @@ package org.example.ui.common.screens
 
 import org.example.logic.use_cases.task_managemnt.GetTaskByStateIdAndProjectId
 import org.example.models.State
+import org.example.ui.common.components.Reader
 import org.example.ui.common.components.UiScreen
 import org.example.ui.common.components.Viewer
 import java.util.*
 
 class ViewStateSelectedForProject(
     private val viewer: Viewer,
-    private val state:State,
+    private val states: List<State>,
     private val projectId: UUID,
     private val getTaskByStateIdAndProjectId: GetTaskByStateIdAndProjectId
 ):UiScreen {
     override fun show() {
 
-        val result = getTaskByStateIdAndProjectId.getTaskByStateIdAndProjectId(projectId, state.id)
         viewer.printTitle("State Details")
-        viewer.printInfoLine("Name: ${state.name}")
-        viewer.printInfoLine("State: $result")
+
+        states.forEachIndexed { index, state ->
+            viewer.printInfoLine("${index + 1}. ${state.name}")
+        }
+        val choice = viewer.readIntInput("Enter the number of the state to view (0 to cancel): ")
+
+        if (choice in 1..states.size) {
+            val selectedState = states.getOrNull(choice?.minus(1) ?: -1)
+            if (selectedState != null) {
+                PrintState(selectedState)
+            } else {
+                viewer.printError("Invalid state selected.")
+            }
+        } else if (choice == 0) {
+            viewer.printInfoLine("Cancelled.")
+        } else {
+            viewer.printError("Invalid choice. Please enter a number between 1 and ${states.size}.")
+        }
+    }
+    private fun PrintState(selectedState : State){
+        val result = getTaskByStateIdAndProjectId.getTaskByStateIdAndProjectId(projectId, selectedState.id)
+        viewer.printTitle("State Details")
+        viewer.printInfoLine("Name: ${selectedState.name}")
+        viewer.printInfoLine("task: $result")
     }
 }
