6067c2946c9041e2a9932f8b5dccd2e2ec95dd88
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-05-05 19:31:18 +0300

    enhanced view projects ui for mate

--- src/main/kotlin/ui/common/screens/ViewProjectsForUserScreen.kt
+++ src/main/kotlin/ui/common/screens/ViewProjectsForUserScreen.kt
@@ -3,17 +3,22 @@ package org.example.ui.common.screens
 import org.example.logic.use_cases.authentication.GetCurrentLoggedInUserUseCase
 import org.example.logic.use_cases.project_manegment.GetProjectByIdUseCase
 import org.example.logic.use_cases.project_manegment.GetProjectsForUserByIdUseCase
+import org.example.logic.use_cases.task_managemnt.CreateTaskUseCase
 import org.example.logic.use_cases.task_managemnt.GetTaskByStateIdAndProjectId
+import org.example.ui.common.components.Reader
 import org.example.ui.common.components.UiScreen
 import org.example.ui.common.components.Viewer
+
 class ViewProjectsForUserScreen(
     private val viewer: Viewer,
+    private val reader: Reader,
     private val getCurrentLoggedInUserUseCase: GetCurrentLoggedInUserUseCase,
-    private val getProjectsForUserById : GetProjectsForUserByIdUseCase,
+    private val getProjectsForUserById: GetProjectsForUserByIdUseCase,
     private val getProjectByIdUserCase: GetProjectByIdUseCase,
-    private val getTaskByStateIdAndProjectId: GetTaskByStateIdAndProjectId
+    private val getTaskByStateIdAndProjectId: GetTaskByStateIdAndProjectId,
+    private val createTaskUseCase: CreateTaskUseCase
+) : UiScreen {
 
-): UiScreen {
     override fun show() {
         val currentUserResult = getCurrentLoggedInUserUseCase.getCurrentUser()
         val user = currentUserResult.getOrNull()
@@ -21,27 +26,36 @@ class ViewProjectsForUserScreen(
         if (user == null) {
             viewer.printError("No user found")
             return
-        }else{
+        } else {
             val userProjectsResult = getProjectsForUserById.getProjectForUserById(user.id)
             userProjectsResult.fold(
-              onSuccess = { projects ->
-                  if (projects.isNotEmpty()) {
-                      viewer.printTitle("Project For User: ${user.name}")
-                      projects.forEachIndexed({ index, project ->
-                          viewer.printInfoLine(
-                              """
+                onSuccess = { projects ->
+                    if (projects.isNotEmpty()) {
+                        viewer.printTitle("Project For User: ${user.name}")
+                        projects.forEachIndexed({ index, project ->
+                            viewer.printInfoLine(
+                                """
                               ${index + 1}-Name Project: ${project.name}
                           """.trimIndent()
-                          )
+                            )
 
-                      })
-                      viewer.printTitle("Select a project to view details:")
-                      val selectedIndex = viewer.readIntInput("Enter project number:")?.minus(1)
-                      val selectedProjectID = projects[selectedIndex!!].id
-                      ViewProjectForUserScreen(selectedProjectID, viewer,getProjectByIdUserCase,getTaskByStateIdAndProjectId).show()
-                  }else
-                      viewer.printInfoLine("No project found for the current user.")
-              } ,
+                        })
+                        viewer.printTitle("Select a project to view details:")
+                        val selectedIndex = viewer.readIntInput("Enter project number:")?.minus(1)
+                        val selectedProjectId = projects[selectedIndex!!].id
+
+                        ViewProjectForUserScreen(
+                            selectedProjectId,
+                            viewer,
+                            reader,
+                            getTaskByStateIdAndProjectId,
+                            getCurrentLoggedInUserUseCase,
+                            createTaskUseCase,
+                            getProjectByIdUserCase
+                        ).show()
+                    } else
+                        viewer.printInfoLine("No project found for the current user.")
+                },
                 onFailure = {
                     viewer.printError("Failed to retrieve projects: ${it.message}")
                 }
