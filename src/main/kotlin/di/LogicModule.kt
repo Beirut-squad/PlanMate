91d38d08d0a15bb3123bedae2c3bb9c0e72067d8
Bassant Mohammed <bassant.22230@gmail.com>
2025-05-05 16:42:31 +0300

    add import will use in create new project screen ui in logic and ui modules

--- src/main/kotlin/di/LogicModule.kt
+++ src/main/kotlin/di/LogicModule.kt
@@ -2,10 +2,12 @@ package org.example.di
 
 import logic.use_cases.log.GetUserProjectLogsUseCase
 import org.example.logic.use_cases.authentication.*
+import logic.use_cases.log.CreateProjectLogUseCase
 import org.example.logic.use_cases.authentication.encryption.EncryptPassword
 import org.example.logic.use_cases.authentication.encryption.Encryptor
 import org.example.logic.use_cases.authentication.encryption.EncryptorMD5Impl
 import org.example.logic.use_cases.log.GetAllProjectLogsUseCase
+import org.example.logic.use_cases.project_manegment.CreateProjectUseCase
 import org.example.logic.use_cases.project_manegment.GetAllProjectsUseCases
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -24,4 +26,6 @@ val logicModule = module {
     singleOf(::GetUserProjectLogsUseCase)
     singleOf(::GetAllProjectsUseCases)
     singleOf(::GetAllProjectLogsUseCase)
+    singleOf(::CreateProjectUseCase)
+    singleOf(::CreateProjectLogUseCase)
 }
