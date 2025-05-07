740963068fca4f2d2170aee2ad4670c607d94ed9
Bassant Mohammed <bassant.22230@gmail.com>
2025-05-07 16:27:41 +0300

    fix: - add user list in model - update project parser, writer, project column index and data module

--- src/main/kotlin/data/csv/project_csv/ProjectCsvParser.kt
+++ src/main/kotlin/data/csv/project_csv/ProjectCsvParser.kt
@@ -4,15 +4,18 @@ import CsvParser
 import org.example.data.csv.smartCsvSplit
 import org.example.models.Project
 import org.example.models.State
+import org.example.models.User
 import java.time.LocalDateTime
 import java.util.*
 
-class ProjectCsvParser(private val stateCsvParser: CsvParser<State>) : CsvParser<Project> {
+class ProjectCsvParser(private val stateCsvParser: CsvParser<State>,
+    private val userCsvParser:  CsvParser<User>) : CsvParser<Project> {
 
 
     override fun parseFile(csvLines: List<String>): List<Project> {
         if (csvLines.isEmpty())
             return emptyList()
+        csvLines.drop(1)
         return csvLines.mapNotNull { parseLine(it) }
     }
 
@@ -24,6 +27,9 @@ class ProjectCsvParser(private val stateCsvParser: CsvParser<State>) : CsvParser
         cleanedLine = line.removeSurrounding("[", "]")
         val parts = smartCsvSplit(cleanedLine)
 
+        if (stateCsvParser.parseLine(parts[ProjectColumnIndex.STATE]) == null){
+            throw Exception("no states in the project")
+        }
         return Project(
             id = UUID.fromString(parts[ProjectColumnIndex.PROJECT_ID]),
             name = parts[ProjectColumnIndex.NAME],
@@ -31,12 +37,16 @@ class ProjectCsvParser(private val stateCsvParser: CsvParser<State>) : CsvParser
             creatorUserID = UUID.fromString(parts[ProjectColumnIndex.CREATOR_USER_ID]),
             createdAt = LocalDateTime.parse(parts[ProjectColumnIndex.CREATED_AT]),
             updatedAt = LocalDateTime.parse(parts[ProjectColumnIndex.UPDATED_AT]),
+            users = parseMultiUser(parts[ProjectColumnIndex.USER]),
             state = parseMultiStates(parts[ProjectColumnIndex.STATE]),
-        )
+            )
     }
-
-    private fun parseMultiStates(line: String): List<State> {
+     private fun parseMultiStates(line: String): List<State> {
         val statesLines = smartCsvSplit(line)
         return stateCsvParser.parseFile(statesLines)
+    }
+    private fun parseMultiUser(line: String): List<User> {
+        val userLines = smartCsvSplit(line)
+        return userCsvParser.parseFile(userLines)
     }
 }
--- src/main/kotlin/di/DataModule.kt
+++ src/main/kotlin/di/DataModule.kt
@@ -1,7 +1,17 @@
 package org.example.di
 
+import data.csv.log_for_project_csv.LogCsvParserForProject
+import data.csv.log_for_project_csv.LogCsvWriterForProject
+import data.csv.project_csv.ProjectCsvParser
+import data.csv.task_csv.TaskCsvParser
+import org.example.data.csv.CsvReader
+import org.example.data.csv.log_for_task_csv.LogCsvParserForTask
+import org.example.data.csv.log_for_task_csv.LogCsvWriterForTask
+import org.example.data.csv.state_csv.StateCsvParser
+import org.example.data.csv.user_csv.UserCsvParser
 import org.example.data.datasource.authentication_data_source.AuthenticationDataSource
 import org.example.data.datasource.log_data_source.LogDataSource
+import org.example.data.datasource.log_data_source.LogDataSourceImpl
 import org.example.data.datasource.project_data_source.ProjectDataSource
 import org.example.data.datasource.task_data_source.TaskDataSource
 import org.example.data.repositories.authentication_repository.AuthenticationRepositoryImpl
@@ -37,6 +47,15 @@ val dataModule = module {
 
     single<AuthenticationRepository> {
         AuthenticationRepositoryImpl(get())
+    }
+
+    single<LogDataSource> {
+        LogDataSourceImpl(
+            csvProjectLogReader = CsvReader(LogCsvParserForProject(ProjectCsvParser(StateCsvParser(), UserCsvParser()))),
+            csvProjectLogWriter = LogCsvWriterForProject(),
+            csvTaskLogReader = CsvReader(LogCsvParserForTask(TaskCsvParser(StateCsvParser()))),
+            csvTaskLogWriter = LogCsvWriterForTask()
+        )
     }
 
     single<LogRepository> {
