48c318c13d8cb6186391314ce17285b524658a10
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-05-12 15:18:21 +0300

    add test for create state use case

---
+++ src/test/kotlin/domain/use_case/state/CreateStateUseCaseTest.kt
@@ -0,0 +1,90 @@
+package domain.use_case.state
+
+import creator_helper.*
+import domain.use_case.authentication.GetCurrentUserUseCase
+import domain.use_case.project.*
+import io.mockk.*
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.*
+import java.util.UUID
+
+class CreateStateUseCaseTest {
+    private val addProjectStateUseCase: AddProjectStateUseCase = mockk(relaxed = true)
+    private val getCurrentUserUseCase: GetCurrentUserUseCase = mockk(relaxed = true)
+    private lateinit var createStateUseCase: CreateStateUseCase
+
+    @BeforeEach
+    fun setUp() {
+        clearAllMocks()
+        createStateUseCase = CreateStateUseCase(addProjectStateUseCase, getCurrentUserUseCase)
+    }
+
+
+    @Test
+    fun `createState should create and return a new state when input is valid`() = runTest {
+        // Given
+        val user = createUserHelper()
+        val project = createProjectHelper()
+        coEvery { getCurrentUserUseCase.getCurrentUser() } returns user
+
+        // When
+        val result = createStateUseCase.createState("Doing", project)
+
+        // Then
+        assertEquals("Doing", result.name)
+        assertNotNull(result.id)
+        coVerify(exactly = 1) {
+            addProjectStateUseCase.addStateToProject(currentUserID = user.id, project = project, state = result)
+        }
+    }
+
+
+    @Test
+    fun `createState should throw IllegalArgumentException when name is blank`() = runTest {
+        // Given
+        val project = createProjectHelper()
+
+        // When & Then
+        val exception = assertThrows<IllegalArgumentException> {
+            createStateUseCase.createState("   ", project)
+        }
+
+        assertEquals("Create failed : name is Blank !!", exception.message)
+        coVerify { getCurrentUserUseCase wasNot Called }
+        coVerify { addProjectStateUseCase wasNot Called }
+    }
+
+//    @Test
+//    fun `createState should throw Exception when current user is null`() = runTest {
+//        // Given
+//        val project = createProjectHelper()
+//        coEvery { getCurrentUserUseCase.getCurrentUser() } returns null
+//
+//        // When & Then
+//        val exception = assertThrows<Exception> {
+//            createStateUseCase.createState("To Do", project)
+//        }
+//
+//        coVerify(exactly = 1) { getCurrentUserUseCase.getCurrentUser() }
+//        coVerify { addProjectStateUseCase wasNot Called }
+//    }
+
+    @Test
+    fun `createState should throw Exception when current user is null`() = runTest {
+        // Given
+        val project = createProjectHelper()
+        coEvery { getCurrentUserUseCase.getCurrentUser() } returns null
+
+        // When & Then
+        val exception = assertThrows<Exception> {
+            createStateUseCase.createState("To Do", project)
+        }
+
+        assertNotNull(exception)
+
+        coVerify(exactly = 1) { getCurrentUserUseCase.getCurrentUser() }
+        coVerify { addProjectStateUseCase wasNot Called }
+    }
+
+}
