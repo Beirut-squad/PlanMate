45e7b32abb05cbffd729256664b039a1b27f4a02
yousef_osama11 <oyousef709@gmail.com>
2025-05-13 15:48:48 +0300

    add test for task logs ui

---
+++ src/test/kotlin/ui/view/user/admin/log/task/TaskLogsUiTest.kt
@@ -0,0 +1,68 @@
+package ui.view.user.admin.log.task
+
+import creator_helper.createTaskLogHelper
+import domain.exception.handler.ExceptionHandler
+import domain.exception.handler.SafeExecutor
+import domain.use_case.authentication.GetUserByIdUseCase
+import domain.use_case.log.GetAllTaskLogsUseCase
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import io.mockk.slot
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import ui.components.Printer
+import ui.view.user.admin.log.task.TaskLogsUi
+
+class TaskLogsUiTest{
+
+    private lateinit var taskLogsUi: TaskLogsUi
+    private lateinit var getAllTaskLogsUseCase: GetAllTaskLogsUseCase
+    private lateinit var getUserByIdUseCase: GetUserByIdUseCase
+    private lateinit var printer: Printer
+    private lateinit var executor: SafeExecutor
+    private lateinit var handler: ExceptionHandler
+
+    @BeforeEach
+    fun setUp() {
+        getAllTaskLogsUseCase = mockk(relaxed = true)
+        getUserByIdUseCase = mockk(relaxed = true)
+        printer = mockk(relaxed = true)
+        executor = mockk(relaxed = true)
+        handler = mockk(relaxed = true)
+
+        taskLogsUi = TaskLogsUi(
+            getAllTaskLogsUseCase,
+            getUserByIdUseCase,
+            printer,
+            executor,
+            handler
+        )
+    }
+
+    @Test
+    fun `should call executor to try to execute`() = runTest {
+        // Given
+        val taskLog = createTaskLogHelper()
+        val taskLogs = listOf(taskLog, taskLog)
+        coEvery { getAllTaskLogsUseCase.getAllTaskLogs() } returns taskLogs
+
+        val actionSlot = slot<suspend () -> List<Any>>()
+        val successSlot = slot<suspend (List<Any>) -> Unit>()
+        val errorSlot = slot<suspend (Throwable) -> Unit>()
+
+        // When
+        taskLogsUi.show()
+
+        // Then
+        coVerify {
+            executor.tryToExecute(
+                action = capture(actionSlot),
+                onSuccess = capture(successSlot),
+                onError = capture(errorSlot)
+            )
+        }
+    }
+}
